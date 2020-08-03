1314062b04f0e6747cc4963b7dac9015f2e420f4
dauntless-panel-9117 <wedderburn95@yahoo.com>
2020-08-03 05:15:18 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,38 @@
 def unsafe?(speed)
+<<<<<<< HEAD
    speed < 40 || speed > 60 ? true : false
 end
 
 def not_safe?(speed)
    speed < 40 || speed > 60 ? true : false
+=======
+   speed < 40|| speed > 60 ? true : false
+end
+
+def not_safe?(speed)
+   speed < 40 || > 60 ? true : false
+end
+
+def sum_machine(first, second)
+   if first < second
+	low = first
+	high=second
+   else
+	low = second
+	high= first
+   end
+
+sum =0
+(low..high).each do |num|
+	sum += num
+   end
+   sum
+end
+
+def squares(collection)
+   collection.collect do |num|
+	num||=0
+	num*num
+   end
+>>>>>>> 5393559a95850a5aecdf431faa59b42c42b0e96d
 end
